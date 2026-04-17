615b0bfdd98c2e3c5bc8327dac43c569dbada144
Lucy <lu.vg0603@gmail.com>
2026-04-17 17:31:57 -0500

    Creación del Pati-menu

--- main.cpp
+++ main.cpp
@@ -9,3 +9,16 @@ struct carro(){
     int fila, columna;
 }
 
+void menu(){
+    int opt;
+    std::cout<<"Bienvenido a tu Pati-adero Cuack"<<std::endl; 
+    std::cout<<"1. Ingresar Pati-vehiculo"<<std::endl;
+    std::cout<<"2. Retirar Pati-vehiculo"<<std::endl;
+    std::cout<<"3. Ver Pati-espacios"<<std::endl;
+    std::cout<<"4. Mostrar Pati-mapa"<<std::endl;
+    std::cout<<"5. Pati-salida";
+    std::cout<<"Selecciona una Pati-opcion: ";
+
+
+}
+
