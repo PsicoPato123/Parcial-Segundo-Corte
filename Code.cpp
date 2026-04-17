4ec323df80805919fdfdfc90fd388b7f5fe72582
PsicoPato123 <lu.vg0603@gmail.com>
2026-04-17 12:28:51 -0500

    Cobro

--- Code.cpp
+++ Code.cpp
@@ -3,4 +3,15 @@
 #include <ctime> //Permite calcular el tiempo de permanencia del vehículo en el parqueadero
 #include <color.h>
 
-//
+//Creación del mapa 
+void estructura mapa(){
+    int x [18][16]{ }
+    std
+
+}
+
+int cobro(int entrada, int salida,){
+    int valor hora= 6000;
+    int total= (salida-entrada)*valor hora;
+    return total;  
+}
