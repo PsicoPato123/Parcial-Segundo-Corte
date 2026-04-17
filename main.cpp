1995f2ced82a47231eca26bf2d30ee3610151410
Lucy <lu.vg0603@gmail.com>
2026-04-17 17:43:07 -0500

    Borrador primer mapa

--- main.cpp
+++ main.cpp
@@ -3,10 +3,34 @@
 #include <ctime>
 #include "color.h"
 
-struct carro(){
+struct carro{
     std::string placa;
     time_t entrada;
     int fila, columna;
+};
+
+void EstructuraMapa(std::string** mapa, int fila, int columna){
+    for(int i = 0; i < fila; i++){
+        for(int j = 0; j < columna; j++){
+
+            // Bordes = vías
+            if(i == 0 || i == fila-1 || j == 0 || j == columna-1){
+                mapa[i][j] = 'Via';
+            }
+            // Calles internas
+            else if(i == fila/2 || j == columna/2){
+                mapa[i][j] = 'Via';
+            }
+            // Parqueaderos
+            else{
+                mapa[i][j] = 'Acceso';
+            }
+        }
+    }
+
+    // Entrada y salida
+    mapa[2][3] = 'E';
+    mapa[fila-1][columna-2] = 'S';
 }
 
 void menu(){
