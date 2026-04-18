b46b44bb94271e1fc718dde9a369ca7afa1d7413
PsicoPato123 <lu.vg0603@gmail.com>
2026-04-17 22:01:48 -0500

    Corrección en 'Estructura Mapa'

--- main.cpp
+++ main.cpp
@@ -18,22 +18,27 @@ void EstructuraMapa(std::string** mapa, int fila, int columna){
             for(int j = 2; j < columna; j += 4){
                 for(int i = 1; i < fila-1; i++){
                 mapa[i][j] = 'Via';
-            }
+            }}
+            //Calles horizontales 
             for(int i= 3; i < fila; i+=4){
                 for(int j = 1; j < columna-1; j ++){      
                 mapa[i][j] = 'Via';
-            }
+            }}
+
             // Espacio de parqueaderos
             for(int i = 1; i < fila-1; i++){
                 for(int j = 1; j < columna-1; j++){
-                    if(mapa[i][j] != 'Via'){
-                        mapa[i][j] = 'Parqueadero';
-                    } }}   
+                    if(mapa[i][j] == 'Calle'){
+                        //declaración de espacios de parqueo
+                        if(mapa[i][j-1] == 'V' || mapa[i][j+1] == 'V' ||
+                    mapa[i-1][j] == 'V' || mapa[i+1][j] == 'V'){
+                    mapa[i][j] = 'P';
+                }}} }
         }}
 
     // Entrada y salida
     mapa[2][3] = 'Entrada';
-    mapa[fila-1][columna-2] = 'Salida';
+    mapa[fila-2][columna-4] = 'Salida';
 }
 
 void menu(){
