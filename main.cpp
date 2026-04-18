f9c16d71ace3ad3b5aa064d70bde894acb2353e4
Lucy <lu.vg0603@gmail.com>
2026-04-17 22:10:50 -0500

    Se realiza corrección de errores generales con ayuda de ChatGPT

--- main.cpp
+++ main.cpp
@@ -14,32 +14,32 @@ void EstructuraMapa(std::string** mapa, int fila, int columna){
     for(int i = 0; i < fila; i++){
         for(int j = 0; j < columna; j++){
             // Bordes conocidos como paredes
-            mapa[i][j] = 'Pared';
+            mapa[i][j] = "Pared";
+        }}
             // Calles verticales
             for(int j = 2; j < columna; j += 4){
                 for(int i = 1; i < fila-1; i++){
-                mapa[i][j] = 'Via';
+                mapa[i][j] = "Via";
             }}
             //Calles horizontales 
             for(int i= 3; i < fila; i+=4){
                 for(int j = 1; j < columna-1; j ++){      
-                mapa[i][j] = 'Via';
+                mapa[i][j] = "Via";
             }}
 
             // Espacio de parqueaderos
             for(int i = 1; i < fila-1; i++){
                 for(int j = 1; j < columna-1; j++){
-                    if(mapa[i][j] == 'Calle'){
+                    if(mapa[i][j] == "Calle"){
                         //declaración de espacios de parqueo
-                        if(mapa[i][j-1] == 'V' || mapa[i][j+1] == 'V' ||
-                    mapa[i-1][j] == 'V' || mapa[i+1][j] == 'V'){
-                    mapa[i][j] = 'P';
+                        if(mapa[i][j-1] == "V" || mapa[i][j+1] == "V" ||
+                    mapa[i-1][j] == "V" || mapa[i+1][j] == "V"){
+                    mapa[i][j] = "P";
                 }}} }
-        }}
 
     // Entrada y salida
-    mapa[2][3] = 'Entrada';
-    mapa[fila-2][columna-4] = 'Salida';
+    mapa[2][3] = "Entrada";
+    mapa[fila-2][columna-4] = "Salida";
 }
 
 void menu(){
